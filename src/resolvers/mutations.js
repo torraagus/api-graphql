175fa794917ed5a09e941d83924f6d25c07a4356
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-20 17:48:10 -0300

    comment line of code to save new user in mongodb

--- src/resolvers/mutations.js
+++ src/resolvers/mutations.js
@@ -1,14 +1,16 @@
 import User from "../models/User";
 import { formatError } from "../errors";
+//import { users } from '../data/users';
 
 export const mutations_r = {
   createTask(_, { input }) {
     return input ? "Task created" : "Error";
   },
-  async createUser(_, { input }) {
+  async createUser(_, { input }, { users }) {
     const newUser = new User(input);
     try {
-      await newUser.save();
+      //await newUser.save(); // save to mongodb in atlas platform
+      users.push(newUser);v // save to users array in memory
       return "User saved!";
     } catch (e) {
       return formatError(e.errors);
