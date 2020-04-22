bd4958f1831e9d8c9e127782170a1527fa05aa64
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-21 21:38:12 -0300

    add mutations for crud users

--- src/schema/mutations.js
+++ src/schema/mutations.js
@@ -1,6 +1,10 @@
 export const mutations = `
     type Mutation {
         createTask(input: TaskInput): String
+
         createUser(input: UserInput): String
+        updateUser(_id: String, user: UserInput): String
+        deleteUser(_id: String): String
     }
 `
+
