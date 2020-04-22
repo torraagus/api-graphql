8ac4bed72da07ab1ab2247f441561aa15cabfba0
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-21 21:37:15 -0300

    import users file directly instead of using ctx

--- src/resolvers/queries.js
+++ src/resolvers/queries.js
@@ -1,8 +1,10 @@
+import users from "../data/users";
+
 export const queries_r = {
   test: () => {
     return "Hello world with graphql";
   },
-  getUsers(root, args , { users }) {
+  getUsers(root, args, ctx) {
     return users;
   },
 };
