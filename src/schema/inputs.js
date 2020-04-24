dc0113899df51ad407ad143073b66510ba314fc3
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-24 12:35:43 -0300

    add input OrderInput

--- src/schema/inputs.js
+++ src/schema/inputs.js
@@ -1,7 +1,13 @@
 import { userInput } from "./entities/user/input";
 import { taskInput } from "./entities/task/input";
 
+export const orderInput = `input OrderInput {
+    sortBy: String!
+    order: String!
+}
+`
 export const inputs = `
     ${userInput}
     ${taskInput}
+    ${orderInput}
 `;
