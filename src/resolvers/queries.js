09dbee45558682d1b9ec506fa40cd414f5fb22fc
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-24 12:34:13 -0300

    update getUsers resolver

--- src/resolvers/queries.js
+++ src/resolvers/queries.js
@@ -1,10 +1,18 @@
 import users from "../data/users";
+import { getUsers } from '../repositories/users';
 
 export const queries_r = {
   test: () => {
     return "Hello world with graphql";
   },
-  getUsers(root, args, ctx) {
-    return users;
+  getUsers(root, { order, skip, limit }, { db, collections: { users } }) {
+    try {
+      return getUsers(users, db, order, skip, limit).then((users) => {
+        if (!users) throw new Error("Error: cannot get users");
+        return users;
+      });
+    } catch (error) {
+      return error;
+    }
   },
 };
