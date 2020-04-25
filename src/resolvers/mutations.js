b17383f73bb110d410b580f1082cc8c4ca02cbf4
broana-ait <torra.agustinignacio@gmail.com>
2020-04-24 21:11:20 -0300

    refactor code

--- src/resolvers/mutations.js
+++ src/resolvers/mutations.js
@@ -1,34 +1,36 @@
 import { createUser, deleteUser, updateUser } from "../repositories/users";
 
+
+
 export const mutations_r = {
   createTask(_root, { input }, ctx) {
     return input ? "Task created" : "Error";
   },
-  createUser(_root, { input }, { db, collections: { users } }, info) {
+  createUser(_root, { input }, ctx, info) {
     try {
-      return createUser(input, users, db).then((userSaved) => {
-        if (!userSaved) throw new Error("Error: User not saved!");
-        return `Success: User saved in ${db}!`;
+      return createUser(input).then((userSaved) => {
+        if (!userSaved) throw new Error("Something went wrong. User not saved!");
+        return `Success: User saved!`;
       });
     } catch (err) {
       return err;
     }
   },
-  updateUser(_root, { _id, user }, { db, collections: { users } }, info) {
+  updateUser(_root, { _id, user }, ctx, info) {
     try {
-      return updateUser({ _id, user }, users, db).then((userSaved) => {
+      return updateUser({ _id, user }).then((userSaved) => {
         if (!userSaved) throw new Error("Error: User not updated!");
-        return `Success: User updated in ${db}!`;
+        return `Success: User updated!`;
       });
     } catch (err) {
       return err;
     }
   },
-  deleteUser(_root, { _id }, { db, collections: { users } }, info) {
+  deleteUser(_root, { _id }, ctx, info) {
     try {
-      return deleteUser(_id, users, db).then((userDeleted) => {
+      return deleteUser(_id).then((userDeleted) => {
         if (!userDeleted) throw new Error("Error: User not deleted!");
-        return `Success: User deleted from ${db}!`;
+        return `Success: User deleted!`;
       });
     } catch (err) {
       return err;
